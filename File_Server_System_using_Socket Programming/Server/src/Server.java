a27062abee16db2f79618472ffed8b9808cdf4c1
gourab98 <gourabsahag@gmail.com>
2021-06-08 12:06:26 +0600

    Server script is added though it is incomplete

--- File_Server_System_using_Socket Programming/Server/src/Server.java
+++ File_Server_System_using_Socket Programming/Server/src/Server.java
@@ -1,2 +1,20 @@
+import javax.swing.*;
+import java.util.ArrayList;
+
 public class Server {
+
+    static ArrayList<MyFile> myFiles = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+        int fileId =0;
+
+        JFrame jFrame = new JFrame("Server");
+        jFrame.setSize(400,400);
+        jFrame.setLayout(new BoxLayout(jFrame.getContentPane(),BoxLayout.Y_AXIS));
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        
+
+    }
 }
