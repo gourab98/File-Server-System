ded63e8b5606546f6549fb6a6dfb59421589d97d
gourab98 <gourabsahag@gmail.com>
2021-06-10 10:32:32 +0600

    Server script is added though it is incomplete 1

--- File_Server_System_using_Socket Programming/Server/src/Server.java
+++ File_Server_System_using_Socket Programming/Server/src/Server.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.net.ServerSocket;
 import java.util.ArrayList;
 
 public class Server {
@@ -14,7 +17,26 @@ public class Server {
         jFrame.setLayout(new BoxLayout(jFrame.getContentPane(),BoxLayout.Y_AXIS));
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        
+        JPanel jPanel = new JPanel();
+        jPanel.setLayout(new BoxLayout(jPanel, BoxLayout.Y_AXIS));
+
+        JScrollPane jScrollPane = new JScrollPane(jPanel);
+        jScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
+        JLabel jlTitle = new JLabel("File Receiver");
+        jlTitle.setFont(new Font("Arial", Font.BOLD, 25));
+        jlTitle.setBorder(new EmptyBorder(20,0,10,0));
+        jlTitle.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        jFrame.add(jlTitle);
+        jFrame.add(jScrollPane);
+        jFrame.setVisible(true);
+
+        ServerSocket serverSocket = new ServerSocket(1212);
+
+        while(true){
+
+        }
 
     }
 }
